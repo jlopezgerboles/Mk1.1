be885d5957c408e00a5a929297a9f088bdf0131f
the_Accountant <93337532+Th3-Accountant@users.noreply.github.com>
2021-11-19 15:29:49 +0100

    Add files via upload

---
+++ testbed/build.bat
@@ -0,0 +1,20 @@
+REM Build script for testbed
+@ECHO OFF
+SetLocal EnableDelayedExpansion
+
+REM Get a list of all the .c files.
+SET cFilenames=
+FOR /R %%f in (*.c) do (
+    SET cFilenames=!cFilenames! %%f
+)
+
+REM echo "Files:" %cFilenames%
+
+SET assembly=testbed
+SET compilerFlags=-g -Wall -Werror
+SET includeFlags=-Isrc -I../systems/engine/
+SET linkerFlags=-L../bin/ -lengine.lib
+SET defines=-D_DEBUG -DKIMPORT
+
+ECHO "Building %assembly%%..."
+clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.exe %defines% %includeFlags% %linkerFlags%
